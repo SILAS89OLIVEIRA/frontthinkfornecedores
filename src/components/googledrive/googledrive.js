dbe089a2b8c64c97b67136f10c00a6af399b5216
LuizToG <luiz.garandy@hotmail.com>
2021-03-08 10:37:59 -0300

    080321

--- src/components/googledrive/googledrive.js
+++ src/components/googledrive/googledrive.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./googledrive.css";
-import { Fade, Button, Row, Col } from 'reactstrap';
+import { FormGroup, Label, Input, Col } from 'reactstrap';
 //import { useEffect } from "react";
-
+import $ from "jquery";
 import MenuSuperior from '../menuSuperior/MenuSuperior';
 
 
@@ -15,87 +15,89 @@ const GoogleDrive = (props) => {
   const [fadeIn3, setFadeIn3] = useState(true);
 
 
-  function toggle1() {
-    var display = document.getElementById('scroll1').style.display;
-    if(display == "none")
-        document.getElementById('scroll1').style.display = 'block';
-    else
-        document.getElementById('scroll1').style.display = 'none';
+
+function toggle1() {
+  var display1 = document.getElementById('scroll1').style.display;
+  if(display1 == "none") {
+      document.getElementById('scroll1').style.display = 'block';
+  } else {
+    document.getElementById('scroll1').style.display = 'none';
+  }
 }
 
 function toggle2() {
-  var display = document.getElementById('scroll2').style.display;
-  if(display == "none")
+  var display2 = document.getElementById('scroll2').style.display;
+  if(display2 == "none") {
       document.getElementById('scroll2').style.display = 'block';
-  else
+  } else { 
       document.getElementById('scroll2').style.display = 'none';
+  }
 }
 
+
 function toggle3() {
-  var display = document.getElementById('scroll3').style.display;
-  if(display == "none")
-      document.getElementById('scroll3').style.display = 'block';
-  else
+  var display3 = document.getElementById('scroll3').style.display;
+  
+  if(display3 == "none") {
+    document.getElementById('scroll3').style.display = 'block';
+  } else {
       document.getElementById('scroll3').style.display = 'none';
+  }
 }
- 
+
+useEffect(() => {
+  $('#select').on('change', function(){
+     var selectValor = $(this).val();
+     console.log(selectValor);
+      $('#fade').children('div').hide();
+      $('#fade').children(selectValor).show();
+  });
+},);
+
+
+
   return (
     <React.Fragment>
 
     <div>
         <MenuSuperior />
-
-
     </div>
     <div >
       <div className="googleDrive">
         <h1 className="tittle">Download de Arquivos</h1>
       </div>
-      <div>
-        <Row>
-          <Col sm={4}>
-            <Button className="buttonIframe1" color="primary" onClick={toggle1}>Arquivos Coleções</Button>
-          </Col>
-          <Col sm={4}>
-            <Button className="buttonIframe2" color="primary" onClick={toggle2}>Arquivos Revista</Button>
-          </Col>
-          <Col sm={4}>
-            <Button className="buttonIframe3" color="primary" onClick={toggle3}>Arquivos Gerais</Button>
-          </Col>
-        </Row>
-
-          <div id="fade">
-          <Row>
-          <Col sm={12} >
-            <Fade in={fadeIn1} tag="h5" className="mt-3">
-              <center >
-                {/*<iframe src="https://googledriveembedder.collegefam.com/?key=AIzaSyBxXtlZCc1ocHHoG6cHDilaxdw2GCYf5Jc&folderid=1z39TV9L1RmOyEkkR2LQqPayk5GIDwYgH"></iframe>*/}
-                <iframe id="scroll1" className="hide" src="https://drive.google.com/embeddedfolderview?id=1lZrt0ocorpEgCty4Zzpw4Q1NwfhieCCS#grid"></iframe>
-              </center>
-            </Fade>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={12}>
-            <Fade in={fadeIn2} tag="h5" className="mt-3">
-              <center>
-                {/*<iframe src="https://googledriveembedder.collegefam.com/?key=AIzaSyBxXtlZCc1ocHHoG6cHDilaxdw2GCYf5Jc&folderid=1z39TV9L1RmOyEkkR2LQqPayk5GIDwYgH"></iframe>*/}
-                  <iframe id="scroll2" className="hide" src="https://drive.google.com/embeddedfolderview?id=1z39TV9L1RmOyEkkR2LQqPayk5GIDwYgH#grid"></iframe>
-              </center>
-            </Fade>
-            </Col>
-        </Row>
-        <Row>
-        <Col sm={12}>
-            <Fade in={fadeIn3} tag="h5" className="mt-3">
-              <center>
-                {/*<iframe src="https://googledriveembedder.collegefam.com/?key=AIzaSyBxXtlZCc1ocHHoG6cHDilaxdw2GCYf5Jc&folderid=1z39TV9L1RmOyEkkR2LQqPayk5GIDwYgH"></iframe>*/}
-                  <iframe id="scroll3" className="hide" src="https://drive.google.com/embeddedfolderview?id=1amU-GM8eXx8bsQHBApDkGLNm25aJAFYa#grid"></iframe>
-              </center>
-            </Fade>
-            </Col>
-        </Row>
-          </div>
+      <div >  
+        <center className="centrocentro">   
+          <FormGroup id='formselect' >
+            <Label for="exampleSelect" className="labelSelect">Tipos de Consultas</Label>
+              <Input type="select" name="select" id="select" >
+                <option value='#div100'>Selecione Uma Opção de Consulta</option>
+                <option value='#div1'>Arquivos Coleções</option>
+                <option value='#div2'>Arquivos Revista</option>
+                <option value='#div3'>Arquivos Gerais</option>
+              </Input>
+          </FormGroup>
+        </center> 
+      </div>
+      <div id="fade">
+        <div id="div1" className="hide">
+          <center >
+            {/*<iframe src="https://googledriveembedder.collegefam.com/?key=AIzaSyBxXtlZCc1ocHHoG6cHDilaxdw2GCYf5Jc&folderid=1z39TV9L1RmOyEkkR2LQqPayk5GIDwYgH"></iframe>*/}
+            <iframe src="https://drive.google.com/embeddedfolderview?id=1lZrt0ocorpEgCty4Zzpw4Q1NwfhieCCS#grid"></iframe>
+          </center>
+        </div>
+        <div id="div2" className="hide">
+          <center >
+            {/*<iframe src="https://googledriveembedder.collegefam.com/?key=AIzaSyBxXtlZCc1ocHHoG6cHDilaxdw2GCYf5Jc&folderid=1z39TV9L1RmOyEkkR2LQqPayk5GIDwYgH"></iframe>*/}
+            <iframe  src="https://drive.google.com/embeddedfolderview?id=1z39TV9L1RmOyEkkR2LQqPayk5GIDwYgH#grid"></iframe>
+          </center>
+        </div>
+        <div id="div3" className="hide">
+          <center>
+            {/*<iframe src="https://googledriveembedder.collegefam.com/?key=AIzaSyBxXtlZCc1ocHHoG6cHDilaxdw2GCYf5Jc&folderid=1z39TV9L1RmOyEkkR2LQqPayk5GIDwYgH"></iframe>*/}
+            <iframe src="https://drive.google.com/embeddedfolderview?id=1amU-GM8eXx8bsQHBApDkGLNm25aJAFYa#grid"></iframe>
+          </center>
+        </div>
       </div>
     </div>
     </React.Fragment>
