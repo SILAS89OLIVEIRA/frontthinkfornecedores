f607c15353768ab2ee695c927a895e846847d823
silas89oliveira <silas.henrique@hotmail.com>
2021-04-07 09:28:11 -0300

    pdfcordova

--- src/components/tabelaFornecedor/TabelaFornecedor.js
+++ src/components/tabelaFornecedor/TabelaFornecedor.js
@@ -253,17 +253,48 @@ const TabelaFornecedor = (props) => {
 
 
     function PDF1() {
-        var pdf = new jsPDF();
+        var doc = new jsPDF();
        
       
-        pdf.autoTable({ html: '#relatorio1' })
+        doc.autoTable({ html: '#relatorio1' })
 
-
+        doc.text(20, 20, 'HELLO!');
+        doc.setFont("courier");
+        doc.setFontType("normal");
+        doc.text(20, 30, 'This is a PDF document generated using JSPDF.');
+        doc.text(20, 50, 'YES, Inside of PhoneGap!');
+        var pdfOutput = doc.output();
+        console.log( pdfOutput );
+        //NEXT SAVE IT TO THE DEVICE'S LOCAL FILE SYSTEM
+        console.log("file system...");
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+           console.log(fileSystem.name);
+           console.log(fileSystem.root.name);
+           console.log(fileSystem.root.fullPath);
+           fileSystem.root.getFile("test.pdf", {create: true}, function(entry) {
+              var fileEntry = entry;
+              console.log(entry);
+              entry.createWriter(function(writer) {
+                 writer.onwrite = function(evt) {
+                 console.log("write success");
+              };
+              console.log("writing to file");
+                 writer.write( pdfOutput );
+              }, function(error) {
+                 console.log(error);
+              });
+           }, function(error){
+              console.log(error);
+           });
+        },
+        function(event){
+         console.log( evt.target.error.code );
+        });
         
 
         
       
-       pdf.save('relatorio1.pdf');
+      // pdf.save('relatorio1.pdf');
    
       };
 
