9830dab6361594ed6ddbbe3c192b0d0d4a3cb971
silas89oliveira <silas.henrique@hotmail.com>
2021-05-30 09:09:31 -0300

    consulta consolidada 300521

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   //  baseURL: 'http://192.168.0.62:3333'
   // baseURL:'http://18.219.112.46:3333'
 
- // baseURL:'http://192.99.210.175:3333'
+ //baseURL:'http://192.99.210.175:3333'
  
 baseURL:'https://api.thinkfornecedores.com.br/'
 
