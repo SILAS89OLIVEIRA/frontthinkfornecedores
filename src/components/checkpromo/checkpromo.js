beab71bcdd7aec9028b3cbdda8d75f41ded0d8f3
GeovannaMiranda <Geovannanm564@outlook.com>
2021-07-15 14:28:56 -0300

    15/07/2021-Componente checkbox

--- src/components/checkpromo/checkpromo.js
+++ src/components/checkpromo/checkpromo.js
@@ -1,34 +1,47 @@
 import React from "react";
 import { Form, FormGroup, Row, Col, Container } from 'reactstrap';
-import { FormControl, InputLabel, Select, MenuItem, Unstable_TrapFocus  } from "@material-ui/core";
+import { FormControl, InputLabel, Select, MenuItem, Unstable_TrapFocus } from "@material-ui/core";
 import MenuSuperior from "../menuSuperior/MenuSuperior";
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { TextField, Button } from "@material-ui/core";
 import { withStyles, makeStyles, lighten } from '@material-ui/core/styles';
-//imports tabela
-//import Unstable_TrapFocus from '@material-ui/core/Unstable_TrapFocus';
-import { DataGrid } from '@material-ui/data-grid';
+
+import { MDBInput, MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
+import Grid from "antd/lib/card/Grid";
+import Card from "reactstrap/lib/Card";
 
 
-const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90,
-    },
-];
-
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-];
 
 
+const data_checkboxes = {
+    columns: [
+        {
+            'check': <MDBInput type="checkbox" id="checkbox2" />,
+            'field': 'check',
+            'sort': 'asc'
+
+        },
+        {
+            'label': 'coluna1',
+            'field': 'lorem',
+            'sort': 'asc'
+        },
+        {
+            'label': 'coluna2',
+            'field': 'lorem',
+            'sort': 'asc',
+        },
+    ],
+    rows: [
+        {
+
+            'check': <MDBInput type="checkbox" id="checkbox2" />,
+            'Descrição': ' ',
+
+        },
+    ]
+};
 
 const CssTextField = withStyles({
     root: {
@@ -64,89 +77,64 @@ const checkpromo = (props) => {
     return (
         <div>
             <MenuSuperior />
-            <div >
-                <Col xl={6}>
-                    <Container className="container-theme" fluid={true}>
-                        <Form >
-                            <Row>
-                                <Col sm={12} md={4} lg={4} xl={4}>
-
-                                    <FormGroup>
-                                        <TextField
-                                            id="standard-multiline-flexible"
-                                            label="De:"
-                                            multiline
-                                            maxRows={4}
-                                            value=""
-                                        />
-                                    </FormGroup>
-                                </Col>
-                                <Col sm={12} md={4} lg={4} xl={4}>
-                                    <FormGroup>
-                                        <TextField
-                                            id="standard-multiline-flexible"
-                                            label="Até:"
-                                            multiline
-                                            maxRows={4}
-                                            value=""
-                                        />
-                                    </FormGroup>
-                                </Col>
-                            </Row>
-                        </Form>
-                        <Form>
-                            <Row>
-                                <Col sm={12} md={4} lg={4} xl={4} >
-                                    <FormGroup >
-                                        <InputLabel id="select-label">Buscar</InputLabel>
-                                        <Select
-                                            labelId="select-label"
-                                            id="select"
-                                            label="Buscar"
-                                        >
-                                            <MenuItem selected disabled value="">Selecione Opção</MenuItem>
-                                            <MenuItem value=""></MenuItem>
-                                            <MenuItem value=""></MenuItem>
-                                            <MenuItem value=""></MenuItem>
-                                        </Select>
-                                    </FormGroup>
-                                </Col>
-                                <Col sm={12}>
-                                    <Button style={{ background: "#007bff", color: "#fff", borderRadius: "0%" }}>Filtrar</Button>{' '}
-                                </Col>
-                            </Row>
-                        </Form>
-                    </Container>
-                </Col>
-            </div>
-            <div>
-                <Container>
+            
+                <center><h1 style={{ marginBottom: '1%', fontFamily: "Roboto, sans-serif", color: '#848484' }}>Produtos de Promoção</h1></center>
+                <Container className="container-theme" fluid={true}>
                     <Row>
-                        <Form>
-                            <Col sm="12" md={{ size: 6, offset: 8 }} >
-                                <FormGroup >
+                        <Col lg={8}>
 
-                                    <Select
-                                        labelId="select-label"
-                                        id="select"
-                                        label="Tipos de Códigos"
-                                    >
-                                        <MenuItem selected disabled value="">Selecione Opção</MenuItem>
-                                        <MenuItem value=""></MenuItem>
-                                        <MenuItem value=""></MenuItem>
-                                        <MenuItem value=""></MenuItem>
-                                    </Select>
-                                </FormGroup>
-                                <div style={{ height: 400, width: '100%' }}>
-                                    <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
-                                </div>
+                            <Card style={{ width: "50%", height: "60%", boxshadow: '#848484' }}>
+                                <Form id="altura">
+                                    <Row>
+                                        <Col sm={12} md={4} lg={4} xl={4} >
+                                            <FormGroup >
+                                                <InputLabel id="select-label"></InputLabel>
+                                                <Select
+                                                    labelId="select-label"
+                                                    id="select"
+                                                    label="Buscar"
+                                                    style={{ width: "410%", margin: '0 0 0 10%' }}
 
-                            </Col>
-                        </Form>
-                    </Row>
-                </Container>
-            </div>
-        </div>
-    );
+                                                >
+                                                    <MenuItem selected disabled value="">Selecione Opção</MenuItem>
+                                                    <MenuItem value=""></MenuItem>
+                                                    <MenuItem value=""></MenuItem>
+                                                    <MenuItem value=""></MenuItem>
+                                                </Select>
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col sm={12}>
+                                            <FormGroup>
+                                                <Button style={{ background: "#007bff", color: "#fff", borderRadius: "0%", width: "40%", height: "2%", margin: '0% 0 0 5%', fontSize: "13px" }}>Filtrar</Button>{' '}
+                                                <Button style={{ background: "#007bff", color: "#fff", borderRadius: "0%", width: "40%", height: "0.5%", margin: '0.5% 0 0 13%', fontSize: "13px" }}>Enviar</Button>{' '}
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </Card>
+
+                        </Col>
+                        <Col lg={4}>
+                           
+                                <Row>
+                                    <Form id="altura">
+
+                                        <Grid>
+                                            <MDBTable bordered >
+                                                <MDBTableHead columns={data_checkboxes.columns} />
+                                                <MDBTableBody rows={data_checkboxes.rows} />
+                                            </MDBTable>
+                                        </Grid>
+
+                                    </Form>
+                                </Row>                           
+                       </Col>
+                </Row>
+            </Container>
+           
+      </div>
+            );
 }
-export default checkpromo;
+            export default checkpromo;
